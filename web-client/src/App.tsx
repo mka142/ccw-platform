4341e00895cf751bd1567a93fe3758a55d555819
Michal <mka@diveinai.com>
2025-10-24 00:27:45 +0200

    feat: implement device management and event fetching in App component

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -9,6 +9,11 @@ import {
 } from "./lib/StateNavigationContext";
 import NoteLoader from "./pages/NoteLoader";
 import Page1 from "./pages/Page1";
+import { useDeviceManager } from "./lib/DeviceManagerClient";
+import { useAutoConnect } from "./hooks/useAutoConnect";
+import Loading from "./components/Loading";
+import { EventSchema } from "./lib/mqtt";
+import config, { EventType } from "./config";
 
 type AppStateType = "INITIALIZATION" | "PAGE1";
 
@@ -17,20 +22,69 @@ interface AppState {
   payload: Record<string, any>;
 }
 
-export function App() {
+const useFetchCurrentEvent = () => {
+  // Fetch the current event from the server
+  const fetcher = async () => {
+    const response = await fetch(config.api.concert.currentEvent);
+    if (!response.ok) {
+      throw new Error("Failed to fetch current event");
+    }
+    const res = await response.json();
+    return res.data as EventSchema<EventType>;
+  };
+  return { fetcher };
+};
+
+const useAppState = () => {
+  const { fetcher: fetchCurrentEvent } = useFetchCurrentEvent();
+  const { latestEvent, connectionStatus, disconnect } =
+    useDeviceManager<EventSchema<EventType>>();
+
   const [state, setState] = useState<AppState>({
     type: "INITIALIZATION",
     payload: {},
   });
 
+  useAutoConnect({
+    brokerUrl: config.mqtt.brokerUrl,
+    topics: [config.mqtt.topics.EVENTS_BROADCAST],
+  });
+
   useEffect(() => {
-    setTimeout(() => {
+    return () => {
+      disconnect();
+    };
+  }, [disconnect]);
+
+  useEffect(() => {
+    if (connectionStatus === "connected" && latestEvent) {
       setState({
-        type: "PAGE1",
-        payload: {},
+        type: latestEvent.eventType,
+        payload: latestEvent.payload,
       });
-    }, 15000); // 15 seconds delay before transitioning to PAGE1 just for demonstration
-  }, []);
+    } else if (connectionStatus === "connected") {
+      //No latest event, fetch from server
+      fetchCurrentEvent()
+        .then((event) => {
+          setState({
+            type: event.eventType,
+            payload: event.payload,
+          });
+        })
+        .catch((err) => {
+          console.error("Failed to fetch current event:", err);
+        });
+    }
+  }, [latestEvent?.eventType, connectionStatus]);
+
+  return { state, connectionStatus };
+};
+
+export function App() {
+  const { state, connectionStatus } = useAppState();
+  if (connectionStatus !== "connected") {
+    return <Loading />;
+  }
 
   return (
     <WithStateNavigation<AppStateType> state={state.type}>
@@ -42,5 +96,9 @@ export function App() {
     </WithStateNavigation>
   );
 }
+
+// export function App() {
+//   return <DeviceManagerExampleWithAutoConnect />;
+// }
 
 export default App;
