c2e95c7fe0a926e406dfbf62998dc1b05bb70782
Michal <mka@diveinai.com>
2025-10-27 03:02:50 +0100

    feat: implement loading state management and transition handling in App component

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "../styles/globals.css";
 import "./index.css";
@@ -10,20 +10,48 @@ import {
 import NoteLoader from "./pages/NoteLoader";
 import Page1 from "./pages/Page1";
 import TensionRecorderPage from "./pages/TensionRecorderPage";
-import Loading from "./components/Loading";
+import { LoadingWithBackgroundTransition } from "./components/Loading";
 
 import { useAppState } from "./hooks/useAppState";
 import type { AppStateType } from "./hooks/useAppState";
+import config from "./config";
 
 export function App() {
-  const { state, connectionStatus } = useAppState();
+  const { state, connectionStatus, userId } = useAppState();
+  const [loadingState, setLoadingState] = useState({
+    shouldBeginTransition: false,
+    transitionFinished: false,
+  });
 
-  if (connectionStatus !== "connected") {
-    return <Loading />;
+  useEffect(() => {
+    if (connectionStatus === "connected" && userId) {
+      setLoadingState((prev) => ({ ...prev, shouldBeginTransition: true }));
+    }
+  }, [connectionStatus, userId]);
+
+  if (
+    connectionStatus !== "connected" ||
+    !userId ||
+    !loadingState.transitionFinished
+  ) {
+    return (
+      <LoadingWithBackgroundTransition
+        finishBackgroundColor={
+          config.constants.pagesBackgroundColor[state.type]
+        }
+        shouldTransitionBegin={loadingState.shouldBeginTransition}
+        setTransitionFinished={(finished) =>
+          setLoadingState((prev) => ({ ...prev, transitionFinished: finished }))
+        }
+      />
+    );
   }
 
   return (
-    <WithStateNavigation<AppStateType> state={state.type}>
+    <WithStateNavigation<AppStateType>
+      state={state.type}
+      stateHash={state.changeId}
+    >
       <StateNavigationPage<AppStateType>
         pageState="INITIALIZATION"
         component={NoteLoader}
